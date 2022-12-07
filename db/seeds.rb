1391f4b94bbfd711f97eeb6473ff74dde06b3185
krisine <kristine.nyaga@student.moringaschool.com>
2022-12-08 01:51:38 +0300

    adds seeding data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+class Seed 
+  def self.begin
+      seed = Seed.new
+      seed.generate_users
+  end
+
+  def generate_users
+      10.times do |i|
+          @user = User.create!(
+              firstname:Faker::Name.name,
+              lastname:Faker::Name.name,
+              email:Faker::Internet.email,
+              password: Faker::Internet.password,
+              description:Faker::Lorem.paragraph(sentence_count: 2),
+              image_url:Faker::Avatar.image
+          )
+          article = Article.create!(
+              user: @user,
+              title:Faker::Movie.title,
+              content:Faker::Lorem.paragraph(sentence_count: 5),
+              category:"Art",
+              rating:Faker::Number.within(range: 1..5),
+              image_url:Faker::Avatar.image
+
+          )
+      end
+  end
+end
+
+Seed.begin
