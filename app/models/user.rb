076e49bfbc2330bac92bc4ac43d9e4183ebc196e
krisine <kristine.nyaga@student.moringaschool.com>
2022-12-08 01:50:37 +0300

    adds validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :articles
+  has_secure_password
+  validates :email,presence: :true
+  validates :firstname,presence: :true
+  validates :lastname,presence: :true
+  validates :image_url,presence: :true
+ 
+
 end
