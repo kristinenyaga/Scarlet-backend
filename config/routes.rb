a8a12c8dc21ce65a5deae46d4a411183f6c5a873
krisine <kristine.nyaga@student.moringaschool.com>
2022-12-08 01:51:04 +0300

    adds routes for articles,users,login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :articles
   resources :users
+  get "/me", to: "users#show"
+  post '/signup', to: 'users#create'
+  delete "/logout", to: "sessions#destroy"
+  post "/login", to: "sessions#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
