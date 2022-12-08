8863b7a4e729612390c3022da67ac3496b95ec25
krisine <kristine.nyaga@student.moringaschool.com>
2022-12-08 13:18:58 +0300

    adding validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
-  
+  validates :strength, inclusion: { in: %w(Business Sports Travel), message: "must be one of Business,Sports,Travel"}
+  validates :title,presence: :true
+  validates :category,presence: :true
+  validates :content,presence: :true
+  validates :image_url,presence: :true
 end
